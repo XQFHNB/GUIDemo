6db8e943a224ee01de13281f4cbf9e7560dff0bd
XQFHNB <1443114135@qq.com>
2016-12-02 12:10:49 +0800

    heh

--- src/com/xqf/container/JFrameDemo.java
+++ src/com/xqf/container/JFrameDemo.java
@@ -1,10 +1,24 @@
 package com.xqf.container;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by XQF on 2016/12/2.
  */
-public class JFrameDemo {
+public class JFrameDemo extends JFrame {
+    //// TODO: 2016/12/2 成员变量的声明，后续添加 
+    public JFrameDemo() {
+        this.setTitle("JFrame 窗口演示");
+        Container contentPane=this.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        // contentPane.add(其他组件)
+        // 其他处理
+        this.setVisible(true);
+        this.setSize(600, 400);
+    }
+
     public static void main(String[] args) {
-        
+        new JFrameDemo();
     }
 }
